f563612b258f2982fb3ef00df6abd10e73790945
joonghyun <joongh@ewhain.net>
2022-08-24 22:14:46 +0900

    Sec 5-2_핸들러 매핑과 핸들러 어댑터

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -6,8 +6,6 @@ import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.rowset.serial.SerialException;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
